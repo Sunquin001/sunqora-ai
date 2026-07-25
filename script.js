1eb26bf011715b25d997fb2d15f3eb2e95a3989e
Jennifer Okangbe <jennieglobal7@gmail.com>
2026-07-25 21:29:47 +0100

    Add revealSections function for scroll animations

--- script.js
+++ script.js
@@ -23,3 +23,28 @@ document.querySelectorAll(".nav-links a").forEach(link => {
     });
 
 });
+const reveals = document.querySelectorAll(".reveal");
+
+function revealSections(){
+
+    reveals.forEach(section=>{
+
+        const windowHeight = window.innerHeight;
+
+        const revealTop = section.getBoundingClientRect().top;
+
+        const revealPoint = 120;
+
+        if(revealTop < windowHeight - revealPoint){
+
+            section.classList.add("active");
+
+        }
+
+    });
+
+}
+
+window.addEventListener("scroll", revealSections);
+
+window.addEventListener("load", revealSections);
